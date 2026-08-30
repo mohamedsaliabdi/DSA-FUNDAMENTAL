198720f55047232318c73adbd5aa4dd59586ab4f
Mohamed Ali <mohamedsaliabdi@gmail.com>
2026-08-30 00:33:52 -0500

    question 2

---
+++ Arithemtic.py
@@ -0,0 +1,19 @@
+a = int(input("enter a"))
+b= int(input("enter b"))
+
+temp = b
+b = a 
+a = temp
+
+sum = a + b
+Diff = a-b
+product = a*b
+quotient = a//b
+print(sum)
+print(Diff)
+print(product)
+print(quotient)
+print(a)
+print(b)
+
+
