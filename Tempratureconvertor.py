52e57e6a5a224d9273c6b4e1d6252a0ff7e7ea9b
Mohamed Ali <mohamedsaliabdi@gmail.com>
2026-09-05 20:26:47 -0500

    temprature

---
+++ Tempratureconvertor.py
@@ -0,0 +1,10 @@
+temp = int(input("Enter temperature in Celsius: "))
+
+
+fahrenheit = (temp * 9/5) + 32
+print(f"{temp}°C is equal to {fahrenheit}°F")
+
+
+kelvin = temp + 273.15
+print(f"{temp}°C is equal to {kelvin}K")
+
