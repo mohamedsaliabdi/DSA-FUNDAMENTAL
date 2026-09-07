09398026aff6eb133b0ec2c4b831e74afe5390c7
Mohamed Ali <mohamedsaliabdi@gmail.com>
2026-09-06 23:57:50 -0500

    pyramidofstars

--- PyramidOfstars.py
+++ PyramidOfstars.py
@@ -9,3 +9,8 @@ for rows in range (1 , N+1):
     for j in range (2*rows-1):
         print("*" , end="")
     print()
+
+# # Outer loop = rows
+# First inner loop = print spaces (N - i)
+# Second inner loop = print stars (2*i - 1)
+# Spaces go down, stars go up
